9e83a80c0d9e65d6f8b7978832700ccf8f080779
Edgar Guamo M <edgarguamo@gmail.com>
2021-11-16 23:19:09 -0500

    Creation of a basic calculator

---
+++ app/src/main/java/com/example/layoutsandbuttons/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.layoutsandbuttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("pipo")
+        clickbutton()
+    }
+
+    private fun clickbutton(){
+        val btnsum = findViewById<Button>(R.id.btn_sum)
+        val btnrest = findViewById<Button>(R.id.btn_rest)
+        val btnmul = findViewById<Button>(R.id.btn_mul)
+        val btndiv = findViewById<Button>(R.id.btn_div)
+        val etnum1 = findViewById<EditText>(R.id.et_num1)
+        val etnum2 = findViewById<EditText>(R.id.et_num2)
+        val tvresult = findViewById<TextView>(R.id.tv_result)
+
+        btnsum.setOnClickListener{tvresult.text = (etnum1.text.toString().toInt() + etnum2.text.toString().toInt()).toString()}
+        btnrest.setOnClickListener{tvresult.text = (etnum1.text.toString().toInt() - etnum2.text.toString().toInt()).toString()}
+        btnmul.setOnClickListener{tvresult.text = (etnum1.text.toString().toInt() * etnum2.text.toString().toInt()).toString()}
+        btndiv.setOnClickListener{tvresult.text = (etnum1.text.toString().toInt() / etnum2.text.toString().toInt()).toString()}
+
+    }
+
+}
